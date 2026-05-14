ae391aa1287c3526d8d5de3f8f9697c8c116a24e
matthewma1522-debug <matthewma1522@gmail.com>
2026-05-14 09:04:32 -0700

    1

--- py.py
+++ py.py
@@ -1,9 +1,10 @@
-
+import random
 import pygame
 pygame.init()
-x,y = 320,240
+player = pygame.Rect(320,240,50,50)
 WIDTH, HEIGHT = 600,400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+coin = pygame.Rect(random.randint(0, WIDTH-20), random.randint(0, HEIGHT-20), 20, 20)
 speed = 5
 running = True
 while running:
@@ -13,18 +14,21 @@ while running:
     
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        x -= speed
+        player.x -= speed
     if keys[pygame.K_RIGHT]:
-        x += speed
+        player.x += speed
     if keys[pygame.K_UP]:
-        y -= speed
+        player.y -= speed
     if keys[pygame.K_DOWN]:
-        y += speed
+        player.y += speed
     
     screen.fill((0,0,0))
-    pygame.draw.rect(screen, (255,0,0), (x,y,50,50))
+    pygame.draw.rect(screen, (255,0,0), (player.x, player.y, 50, 50))
+    pygame.draw.rect(screen, (255,255,0), coin)
     pygame.display.update()
 
-
+    if player.colliderect(coin):
+        coin.x = random.randint(0, WIDTH-20)
+        coin.y = random.randint(0, HEIGHT-20)
 
 pygame.quit()
